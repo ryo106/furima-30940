e0e28a26b9ea9754c77dcce8c3426b0ecf676953
ryo106 <s86.shimauchi.ryosuke@gmail.com>
2020-12-08 21:48:27 +0900

    cssファイル取込、itemsコントローラー、ビュー、モデル（migration空カラム状態）の作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  # get 'items/index'
+  # get 'items/new'
+  # get 'items/show'
+  # get 'items/edit'
+  # get 'index/new'
+  # get 'index/show'
+  # get 'index/edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "items#index"
 end
