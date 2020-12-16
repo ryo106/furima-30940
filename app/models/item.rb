a39194c0f182dac85bcaab7a5121b91144cbc233
ryo106 <s86.shimauchi.ryosuke@gmail.com>
2020-12-16 11:49:45 +0900

    商品出品機能のコミット

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,26 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  has_many :orders
+  has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :product_status
+  belongs_to :shipping_fee_burden
+  belongs_to :prefecture
+  belongs_to :delivery_day
+
+  with_options presence: true do
+    validates :image
+    validates :title
+    validates :text
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }, format: { with: /\A[0-9]+\z/ }
+    with_options numericality: { other_than: 1, message: "select" } do
+      validates :category_id
+      validates :product_status_id
+      validates :shipping_fee_burden_id
+      validates :prefectures_id
+      validates :delivery_days_id
+    end
+  end
 end
---
+++ spec/models/item_spec.rb
@@ -0,0 +1,74 @@
+require 'rails_helper'
+
+RSpec.describe Item, type: :model do
+  before do
+    @item = FactoryBot.build(:item)
+  end
+  describe '商品出品情報' do
+    it "全ての商品出品情報が正しく入力されている場合、登録できること" do
+      expect(@item).to be_valid
+    end
+    it '商品画像を1枚つけることが必須であること' do
+      @item.image = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include "Image can't be blank"
+    end
+    it '商品名が必須であること' do
+      @item.title = ''  
+      @item.valid?
+      expect(@item.errors.full_messages).to include "Title can't be blank"
+    end
+    it '商品の説明が必須であること' do
+      @item.text = ''  
+      @item.valid?
+      expect(@item.errors.full_messages).to include "Text can't be blank"
+    end
+    it '商品の状態が必須であること' do
+      @item.product_status_id = 1  
+      @item.valid?
+      expect(@item.errors.full_messages).to include "Product status select"
+    end
+    it 'カテゴリーの情報が必須であること' do
+      @item.category_id = 1  
+      @item.valid?
+      expect(@item.errors.full_messages).to include "Category select"
+    end
+    it '配送料の負担についての情報が必須であること' do
+      @item.shipping_fee_burden_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include "Shipping fee burden select"
+    end
+    it '発送元の地域についての情報が必須であること' do
+      @item.prefectures_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include "Prefectures select"
+    end
+    it '発送までの日数についての情報が必須であること' do
+      @item.delivery_days_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include "Delivery days select"
+    end
+    it '価格についての情報が必須であること' do
+      @item.price = ''  
+      @item.valid?
+      expect(@item.errors.full_messages).to include "Price can't be blank"
+    end
+    it '価格の範囲が、¥300~¥9,999,999の間であること' do
+      @item.price = 299
+      @item.valid?
+      expect(@item.errors.full_messages).to include "Price must be greater than or equal to 300"
+      
+    end
+    it '価格の範囲が、¥300~¥9,999,999の間であること' do
+      @item.price = 10000000
+      @item.valid?
+      expect(@item.errors.full_messages).to include "Price must be less than or equal to 9999999"
+      
+    end
+    it '販売価格は半角数字のみ保存可能であること' do
+      @item.price = '５０００'
+      @item.valid?
+      expect(@item.errors.full_messages).to include "Price is not a number"
+    end
+  end
+end
