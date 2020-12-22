111d9fc9233991f8a17384558b7cd4d3e1c4f1f7
ryo106 <s86.shimauchi.ryosuke@gmail.com>
2020-12-22 20:12:57 +0900

    商品購入機能の再提出3

--- spec/factories/order_address.rb
+++ spec/factories/order_address.rb
@@ -4,6 +4,7 @@ FactoryBot.define do
     prefecture_id                    { 2 }
     municipality                     { '全角' }
     address                          { '青山1-1-1' }
+    building_name                    { '建物名'}
     phone_number                     { '09054383161' }
     token                            { 'tok_abcdefghijk00000000000000000' }
 
